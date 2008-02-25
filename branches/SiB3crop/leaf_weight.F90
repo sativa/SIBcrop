079dd3dec2ff9f7af9129921857fac595bb3efe8
baker <baker@28913066-e713-0410-bcd9-9ccb7d0417a0>
2008-02-25 23:23:39 +0000

    added leaf_weight to the repository list

---
+++ branches/SiB3crop/leaf_weight.F90
@@ -0,0 +1,82 @@
+!================ SUBROUTINE LEAF WEIGHT ===============
+
+
+!itb_crop...Short program to determine final leaf weight,
+!itb_crop...based on crop, time since planting, and 
+!itb_crop...cumulative dry weight
+
+   subroutine leaf_weight(sib,time)
+
+use kinds
+use sibtype
+use timetype
+
+!------------------------------------------------------------------
+type(sib_t), intent(inout) :: sib
+type(time_struct), intent(in) :: time
+!------------------------------------------------------------------
+
+
+!itb_crop...CORN
+
+   if(sib%param%biome == 20.0) then
+
+      if( sib%diag%gdd == 0.0 ) then
+
+           sib%diag%leafwt_c =  0.01 
+      
+      elseif (sib%diag%gdd > 0.0001 .and. sib%diag%gdd < 2500) then
+
+	       sib%diag%leafwt_c = sib%diag%cum_drywt(2)
+	
+      elseif (sib%diag%gdd >= 2500.0 .and. sib%diag%gdd < 2950.0) then
+
+          sib%diag%leafwt_c = 0.95 * sib%diag%cum_drywt(2) -   &
+          (0.95-0.1) * sib%diag%cum_drywt(2) *      &
+                              ((sib%diag%gdd - 2500.0) / 450.0)
+       
+      elseif( sib%diag%gdd >= 2950.0 .or. time%doy>=sib%diag%pd+175) then
+
+          sib%diag%leafwt_c=sib%diag%cum_drywt(2)*0.01
+
+      endif
+
+!itb_crop...SOY
+   elseif(sib%param%biome == 21.0) then
+
+      if (sib%diag%pd > 0                     .AND.      &
+         time%doy    >= (sib%diag%pd+10)     .AND.      &
+         time%doy    <  (sib%diag%pd+75))    then
+
+	       sib%diag%leafwt_c = sib%diag%cum_drywt(2)
+	
+      elseif (sib%diag%pd >  0                    .AND.      &
+             time%doy    >= (sib%diag%pd+75)     .AND.  &
+              time%doy   <  (sib%diag%pd+100))   then
+
+	       sib%diag%leafwt_c = sib%diag%cum_drywt(2) * 0.85 
+ 
+      elseif (sib%diag%pd >  0                    .AND.      &
+             time%doy    >= (sib%diag%pd+100)    .AND.      &
+             time%doy    <  (sib%diag%pd+121))   then
+
+               x=(time%doy - (sib%diag%pd+100)) / 21.0
+
+	           sib%diag%leafwt_c = 0.85 * sib%diag%cum_drywt(2) -   &
+                 (0.85-0.001) * sib%diag%cum_drywt(2) * x 
+
+       else
+
+	       sib%diag%leafwt_c = sib%diag%cum_drywt(2) * 0.001
+
+       endif
+
+   else
+
+      print*,'INCORRECT CROP TYPE SELECTED'
+      print*,'Biome Number=',sib%param%biome
+      stop'SUBROUTINE LEAF_WEIGHT'
+   endif
+
+   end subroutine leaf_weight
+
